6e530b6b85d017d097e2c8b5c1d2b0c27d07f41b
mlgvla <mlgerardvla@gmail.com>
2021-08-09 01:30:24 -0400

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,18 @@
-// write your CatList component here
+import React, { Component } from 'react';
+
+class CatList extends Component {
+    catPix = () => {
+        return this.props.catPics.map(catPic => <img key={catPic.id} src={catPic.url} alt={catPic.id}/>)
+    }
+    render() {
+        return (
+            <div>
+              {this.catPix()}  
+            </div>
+        );
+    }
+}
+
+export default CatList;
+
+
--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,24 @@
-// add the catsReducer
+const catsReducer = (state = {
+    cats: [],
+    loading: false
+    }, action) => {
+
+        switch (action.type) {
+            case 'LOADING_CATS':
+                return {
+                    ...state,
+                    cats: [...state.cats],
+                    loading: true
+                }
+            case 'ADD_CATS':
+                return {
+                    ...state,
+                    cats: action.cats, //state preservation must be handles in action creator
+                    loading: false
+                }   
+        
+            default:
+                return state;
+        }
+    }
+export default catsReducer
